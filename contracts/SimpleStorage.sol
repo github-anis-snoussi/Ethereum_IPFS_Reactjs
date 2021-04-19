0b19b1fc18b184a7361a0907ba722137c146a95b
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-04-19 15:35:41 -0400

    can migrate

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -3,11 +3,11 @@ pragma solidity 0.5.16;
 contract SimpleStorage {
   string ipfsHash;
 
-  function set(string x) public {
+  function set(string memory x) public {
     ipfsHash = x;
   }
 
-  function get() public view returns (string) {
+  function get() public view returns (string memory) {
     return ipfsHash;
   }
 }
