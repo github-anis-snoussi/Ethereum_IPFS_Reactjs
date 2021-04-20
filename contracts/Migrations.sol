c92fd73acdb7819f4749659ef563e10bfe2682a9
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-04-20 09:14:23 -0400

    updated smart contract

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  string public last_completed_migration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
@@ -13,7 +13,7 @@ contract Migrations {
     owner = msg.sender;
   }
 
-  function setCompleted(uint completed) public restricted {
+  function setCompleted(string memory completed) public restricted {
     last_completed_migration = completed;
   }
 }
--- test/simplestorage.js
+++ test/simplestorage.js
@@ -1,15 +1,15 @@
 const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 contract("SimpleStorage", accounts => {
-  it("...should store the value 89.", async () => {
+  it("...should store the value 'Hello World'.", async () => {
     const simpleStorageInstance = await SimpleStorage.deployed();
 
-    // Set value of 89
-    await simpleStorageInstance.set(89, { from: accounts[0] });
+    // Set value of 'Hello World'
+    await simpleStorageInstance.set('Hello World', { from: accounts[0] });
 
     // Get stored value
     const storedData = await simpleStorageInstance.get.call();
 
-    assert.equal(storedData, 89, "The value 89 was not stored.");
+    assert.equal(storedData, 'Hello World', "The value 'Hello World' was not stored.");
   });
 });
