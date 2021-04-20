0e14fa4330f74203c14c3cc9eb6d55736ed4d349
Anis Snoussi <snoussi.anis@insat.u-carthage.tn>
2021-04-20 09:16:28 -0400

    updated wrong contract, lol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.21 <0.7.0;
 
 contract Migrations {
   address public owner;
-  string public last_completed_migration;
+  uint public last_completed_migration;
 
   modifier restricted() {
     if (msg.sender == owner) _;
@@ -13,7 +13,7 @@ contract Migrations {
     owner = msg.sender;
   }
 
-  function setCompleted(string memory completed) public restricted {
+  function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
   }
 }
--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleStorage {
-  uint ipfsHash;
+  string ipfsHash;
 
-  function set(uint x) public {
+  function set(string memory x) public {
     ipfsHash = x;
   }
 
-  function get() public view returns (uint) {
+  function get() public view returns (string memory) {
     return ipfsHash;
   }
 }
